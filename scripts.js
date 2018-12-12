63bea54277aa5e49000130e80cf6e9e14fb8820b
Maggie D <45428880+maggiewld@users.noreply.github.com>
2018-12-12 18:16:37 +0000

    Add variable, edit sequence of code to fix runtime errors.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
-//Javascript statements
-alert("Hello");
+var message = "Hello!";
+alert(message);
+message = "Welcome to Javascript Basics";
+alert(message);
 document.write("<h1>Welcome to Javascript Basics</h1>");
 alert("Thanks for visiting.");
-console.log("Program complete"); //Shows a message in JS console
+
+ console.log("Program complete"); //Shows a message in JS  console
