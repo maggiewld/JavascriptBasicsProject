46e792ec434b565ed8de1bf42d75b76a48260b78
Maggie D <45428880+maggiewld@users.noreply.github.com>
2018-12-14 11:53:58 +0000

    Add test prompt variable and print return value.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 var message = "Hello!";
 alert(message);
-message = "Welcome to Javascript Basics";
-alert(message);
-document.write("<h1>Welcome to Javascript Basics</h1>");
-alert("Thanks for visiting.");
+var visitorName = prompt("What is your name?"); //Captures user input/returned value
+alert(visitorName);
+//message = "Welcome to Javascript Basics";
+//document.write("<h1>Welcome to Javascript Basics</h1>");
+//alert("Thanks for visiting.");
 
  console.log("Program complete"); //Shows a message in JS  console
