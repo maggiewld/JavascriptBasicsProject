445c6c426341537e97e8c0320ce3cac106ba33fd
Maggie D <45428880+maggiewld@users.noreply.github.com>
2018-12-14 12:50:16 +0000

    Clean up comments.

--- scripts.js
+++ scripts.js
@@ -6,8 +6,10 @@
 //alert("Thanks for visiting.");
 //console.log("Program complete"); //Shows a message in JS  console
 
-var visitor = prompt("What is your name?");
-var message = "Hello " + visitor; // + combines strings (Concatenation). Spaces need to be added within the string quotes.
-message = message + ". Welcome to Treehouse.";
-message += " We\'re so glad that you came by."; //'+=' same as writing out '= message'. Will update from previous.
-document.write(message);
+//var visitor = prompt("What is your name?");
+//var message = "Hello " + visitor;
+    // + combines strings   (Concatenation). Spaces need to be added within the string quotes.
+//message = message + ". Welcome to Treehouse.";
+//message += " We\'re so glad that you came by.";
+    //'+=' same as writing out '= message'. Will update from previous.
+//document.write(message);
