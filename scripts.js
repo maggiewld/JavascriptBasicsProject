d511954ed9be96dcd719d71ee14cef0e40c3c69c
Maggie D <45428880+maggiewld@users.noreply.github.com>
2018-12-14 13:32:34 +0000

    Edit and create story maker.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,43 @@
-//alert(message);
-//var visitorName = prompt("What is your name?"); //Captures user input/returned value
-//alert(visitorName);
-//message = "Welcome to Javascript Basics";
-//document.write("<h1>Welcome to Javascript Basics</h1>");
-//alert("Thanks for visiting.");
-//console.log("Program complete"); //Shows a message in JS  console
+//Basics---------------------------------------------------
+/*
+alert(message);
+var visitorName = prompt("What is your name?"); Captures user input/returned value
+alert(visitorName);
+message = "Welcome to Javascript Basics";
+document.write("<h1>Welcome to Javascript Basics</h1>");
+alert("Thanks for visiting.");
+console.log("Program complete"); //Shows a message in JS  console
+*/
 
-//var visitor = prompt("What is your name?");
-//var message = "Hello " + visitor;
+//Concatenation--------------------------------------------
+/*
+var visitor = prompt("What is your name?");
+var message = "Hello " + visitor;
     // + combines strings   (Concatenation). Spaces need to be added within the string quotes.
-//message = message + ". Welcome to Treehouse.";
-//message += " We\'re so glad that you came by.";
+message = message + ". Welcome to Treehouse.";
+message += " We\'re so glad that you came by.";
     //'+=' same as writing out '= message'. Will update from previous.
-//document.write(message);
+document.write(message);
+*/
+
+//Example Method-------------------------------------------
+/*
+var stringToShout = prompt("What should I shout?");
+var shout = stringToShout.toUpperCase();
+shout += "!!!";
+alert(shout);
+*/
+
+//Story Maker Code-----------------------------------------
+"<h2> There once was a [Adjective] [Noun] that was the [Adjective] in the world!</h2>"
+
+alert("Time to write a story!")
+var firstWord = prompt("Input an adjective");
+var secondWord = prompt ("Input a noun");
+var thirdWord = prompt ("Input another adjective")
+alert("All done!")
+var message = "<h2>There once was a " + firstWord
+message += " " + secondWord;
+message += " that was the " + thirdWord;
+message += " " + secondWord + " in the world!</h2>"
+document.write(message);
