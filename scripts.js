1cb763e0bdc5caca59f2ce712db00024850403f7
Maggie D <45428880+maggiewld@users.noreply.github.com>
2018-12-14 16:53:39 +0000

    Test string to integer conversion.

--- scripts.js
+++ scripts.js
@@ -45,6 +45,7 @@ document.write(message);
 */
 
 //Time Calculator Code ------------------------------------
+/*
 var secondsPerMin = 60;
 var minsPerHour = 60;
 var hoursPerDay = 24;
@@ -57,3 +58,12 @@ var weeksAlive = yearsAlive * weeksPerYear;
 var daysAlive = weeksAlive * daysPerWeek;
 var secondsAlive = daysAlive * secondsPerDay;
 document.write(" I\'ve been alive for more than " + secondsAlive + " seconds.");
+*/
+
+//Converting strings to numbers----------------------------
+/*
+var htmlBadges = prompt("How many HTML badges do you have?");
+var cssBadges = prompt("How many CSS badges do you have?");
+var totalBadges = parseInt(htmlBadges) + parseInt (cssBadges); //parseInt to convert string to integer
+alert("Wow! You have " + totalBadges + " badges!");
+*/
