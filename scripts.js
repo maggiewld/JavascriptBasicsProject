c9e7f2286dfd4f1c1dd4424c6eb9a4dbb9bf77ff
Maggie D <45428880+maggiewld@users.noreply.github.com>
2018-12-14 12:37:28 +0000

    Test string concatenation.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
-var message = "Hello!";
-alert(message);
-var visitorName = prompt("What is your name?"); //Captures user input/returned value
-alert(visitorName);
+//alert(message);
+//var visitorName = prompt("What is your name?"); //Captures user input/returned value
+//alert(visitorName);
 //message = "Welcome to Javascript Basics";
 //document.write("<h1>Welcome to Javascript Basics</h1>");
 //alert("Thanks for visiting.");
+//console.log("Program complete"); //Shows a message in JS  console
 
- console.log("Program complete"); //Shows a message in JS  console
+var visitor = prompt("What is your name?");
+var message = "Hello " + visitor; // + combines strings (Concatenation). Spaces need to be added within the string quotes.
+message = message + ". Welcome to Treehouse.";
+message += " We\'re so glad that you came by."; //'+=' same as writing out '= message'. Will update from previous.
+document.write(message);
