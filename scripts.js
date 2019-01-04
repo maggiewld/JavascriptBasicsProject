f9d44415628a5f10d39e01af416fed58ea97e1b6
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-01-04 10:38:44 +0000

    Add 'else if' conditions for 'random number game'.

--- scripts.js
+++ scripts.js
@@ -107,9 +107,20 @@ var randomNumber = Math.floor(Math.random() * 6 ) + 1;
 var guess = prompt("I'm thinking of a number between 1 and 6. What is it?");
 if (parseInt(guess) === randomNumber) {
   correctGuess = true;
+} else if (parseInt(guess) < randomNumber) {
+  var guessMore = prompt("Try again. The number I am thinking of is more than " + guess);
+    if (parseInt(guessMore) === randomNumber) {
+      correctGuess = true;
+    }
+} else if (parseInt(guess) > randomNumber) {
+  var guessLess = prompt("Try again. The number I am thinking of is less than " + guess);
+    if (parseInt(guess) === randomNumber) {
+      correctGuess = true;
+    }
 }
-  if (correctGuess === true) {
-    document.write("<p>You guessed the number!</p>");
-  } else {
-    document.write("<p>Sorry. The number was " + randomNumber ".</p>");
-  }
+
+if (correctGuess) {
+  document.write("<p>You guessed the number!</p>");
+} else {
+  document.write("<p>Sorry. The number was " + randomNumber + ".</p>");
+}
